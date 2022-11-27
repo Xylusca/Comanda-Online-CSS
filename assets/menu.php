49502d91a728fe46f3e6399486e52f44a70a23e3
Xylusca <lukas_sud@live.com>
2022-11-27 09:56:46 -0300

    Teste

---
+++ assets/menu.php
@@ -0,0 +1,38 @@
+<main>
+
+    <div class="menu menu--closed">
+
+        <div class="button">
+            <span> </span>
+            <span> </span>
+            <span> </span>
+        </div>
+
+        <div class="tools tools--hidden">
+            
+            <!-- Carrinho -->
+            <a href="#comanda">
+                <i class='bx bx-cart-alt'></i>
+            </a>
+            
+            <!-- Contato -->
+            <a href="#">
+                <i class='bx bx-phone-call'></i>
+            </a>
+            
+            <!-- Avaliação -->
+            <a href="#">
+                <i class='bx bx-star'></i>
+            </a>
+            
+            <!-- login -->
+            <a href="#">
+                <i class='bx bx-log-in'></i>
+            </a>
+
+
+        </div>
+
+    </div>
+
+</main>
